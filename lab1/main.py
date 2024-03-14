a996d872fb291784f823ae0e0f745bc10bc7dd52
Serafim Lyovin <87949621+zxcvve@users.noreply.github.com>
2024-03-14 18:59:30 +0300

    Formatted using black

--- lab1/main.py
+++ lab1/main.py
@@ -1,22 +1,36 @@
 import matplotlib.pyplot as plt
 from collections import Counter
 
+
 def analyze_file(file_path):
-    with open(file_path, 'rb') as file:
+    with open(file_path, "rb") as file:
         content = file.read()
     frequency = Counter(content)
     return frequency
+
 
 def plot_histogram(frequency):
     labels, values = zip(*frequency.items())
     plt.bar(labels, values)
     plt.xlim(0, 255)
     plt.ylim(0, max(values))
-    plt.xlabel('ASCII value')
-    plt.ylabel('Frequency')
-    plt.title('Frequency Analysis')
+    plt.xlabel("ASCII value")
+    plt.ylabel("Frequency")
+    plt.title("Frequency Analysis")
     plt.show()
 
-# Пример использования
-frequency = analyze_file('a.txt')
+
+available_bmp = [
+    "./materials/bmp/blackbuck.bmp",
+    "./materials/bmp/bmp_08.bmp",
+    "./materials/bmp/bmp_25.bmp",
+    "./materials/bmp/snail.bmp",
+]
+available_txt = [
+    "./materials/txt/1.txt",
+    "./materials/txt/2.txt",
+    "./materials/txt/3.txt",
+    "./materials/txt/4.txt",
+]
+frequency = analyze_file(available_bmp[1])
 plot_histogram(frequency)
