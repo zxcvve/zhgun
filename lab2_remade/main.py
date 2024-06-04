3e69aa90188c23401464b5112c5c8ac2fd830afb
Serafim Lyovin <87949621+zxcvve@users.noreply.github.com>
2024-06-04 21:43:39 +0300

    Remove unused imports

--- lab2_remade/main.py
+++ lab2_remade/main.py
@@ -1,11 +1,6 @@
-import codecs
-from sys import getdefaultencoding
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
-import sympy
 from pylfsr import LFSR
-import struct
 
 
 def lcg(x, a, c, m):
@@ -77,76 +72,79 @@ def visualize(array):
     plt.show()
 
 
-# rus = random_uniform_sample(50, [0, 255])
-# print(rus)
-# print("LCGx50")
-# visualize(rus)
+rus = random_uniform_sample(50, [0, 255])
+print(rus)
+print("LCGx50")
+visualize(rus)
 
-# rus = random_uniform_sample(100, [0, 255])
-# print(rus)
-# print('LCGx100')
-# visualize(rus)
+rus = random_uniform_sample(100, [0, 255])
+print(rus)
+print('LCGx100')
+visualize(rus)
 
-# rus = random_uniform_sample(1000, [0, 255])
-# print(rus)
-# print('LCGx1000')
-# visualize(rus)
+rus = random_uniform_sample(1000, [0, 255])
+print(rus)
+print('LCGx1000')
+visualize(rus)
 
-# bb = BlumBlumShub(50)
-# s = bb.get_random_bits()
-# print(s)
-# bl = list(int(s[i : i + 8], 2) for i in range(0, len(s), 8))
-# #bl = bitstring_to_bytes(s)
-# print(bl)
-# print('BlumBlumShub x 50')
-# visualize(bl)
+bb = BlumBlumShub(50)
+s = bb.get_random_bits()
+print(s)
+bl = list(int(s[i : i + 8], 2) for i in range(0, len(s), 8))
+#bl = bitstring_to_bytes(s)
+print(bl)
+print('BlumBlumShub x 50')
+visualize(bl)
 
-# bb = BlumBlumShub(100)
-# s = bb.get_random_bits()
-# print(s)
-# bl = list(int(s[i : i + 8], 2) for i in range(0, len(s), 8))
-# print(bl)
-# print('BlumBlumShub x 100')
-# visualize(bl)
+bb = BlumBlumShub(100)
+s = bb.get_random_bits()
+print(s)
+bl = list(int(s[i : i + 8], 2) for i in range(0, len(s), 8))
+print(bl)
+print('BlumBlumShub x 100')
+visualize(bl)
 
-# bb = BlumBlumShub(1000)
-# s = bb.get_random_bits()
-# print(s)
-# bl = list(int(s[i : i + 8], 2) for i in range(0, len(s), 8))
-# print(bl)
-# print('BlumBlumShub x 1000')
-# visualize(bl)
+bb = BlumBlumShub(1000)
+s = bb.get_random_bits()
+print(s)
+bl = list(int(s[i : i + 8], 2) for i in range(0, len(s), 8))
+print(bl)
+print('BlumBlumShub x 1000')
+visualize(bl)
 
 
-# fpoly = [5,2]
-# L = LFSR(fpoly=fpoly, initstate='random')
+fpoly = [5,2]
+L = LFSR(fpoly=fpoly, initstate='random')
 
-# # Generate K-bits
-# k=50*8
-# seq_k  = L.runKCycle(k)
+# Generate K-bits
+k=50*8
+seq_k  = L.runKCycle(k)
 
-# print('bits')
-# print(L.arr2str(seq_k))
+print('bits')
+print(L.arr2str(seq_k))
 
-# bl = list(int(str(seq_k[i : i + 8]).replace(']','').replace('[','').replace(' ',''), 2) for i in range(0, len(seq_k), 8))
-# print(bl)
-# print('LFSR x 50')
-# visualize(bl)
+bl = list(int(str(seq_k[i : i + 8]).replace(']','').replace('[','').replace(' ',''), 2) for i in range(0, len(seq_k), 8))
+print(bl)
+print('LFSR x 50')
+visualize(bl)
 
-# fpoly = [5,2]
-# L = LFSR(fpoly=fpoly, initstate='random')
+fpoly = [5,2]
+L = LFSR(fpoly=fpoly, initstate='random')
 
-# # Generate K-bits
-# k=100*8
-# seq_k  = L.runKCycle(k)
+# Generate K-bits
+k=100*8
+seq_k  = L.runKCycle(k)
 
-# print('bits')
-# print(L.arr2str(seq_k))
+print('bits')
+print(L.arr2str(seq_k))
 
-# bl = list(int(str(seq_k[i : i + 8]).replace(']','').replace('[','').replace(' ',''), 2) for i in range(0, len(seq_k), 8))
-# print(bl)
-# print('LFSR x 100')
-# visualize(bl)
+bl = list(int(str(seq_k[i : i + 8]).replace(']','').replace('[','').replace(' ',''), 2) for i in range(0, len(seq_k), 8))
+print(bl)
+print('LFSR x 100')
+visualize(bl)
+
+
+
 
 fpoly = [5,2]
 L = LFSR(fpoly=fpoly, initstate='random')
