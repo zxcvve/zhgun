cbced928d99e9b4fbc5eb34a8b1e66170267bbc9
Serafim Lyovin <87949621+zxcvve@users.noreply.github.com>
2024-06-04 23:01:32 +0300

    Add lab3 solution

--- lab3/main.py
+++ lab3/main.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from pylfsr import LFSR
 from collections import Counter
 import numpy as np
 import os
@@ -23,7 +24,7 @@ class VigenereCipher:
         for i in range(len(self.text)):
             text_char = self.text[i]
             password_char = self.extended_password[i]
-            if text_char.isalpha():  # Only encrypt alphabetic characters
+            if text_char.isalpha():  
                 shift = ord(password_char.lower()) - ord('а')
                 if text_char.islower():
                     encrypted_char = chr((ord(text_char) - ord('а') + shift) % 32 + ord('а'))
@@ -49,10 +50,44 @@ def plot_histogram(text_bin):
     plt.show()
 
 file_path = f"{script_dir}/input.txt" 
+
+# first task
 password = "Поле глазасто, а лес ушаст."
 text_binary = read_file(file_path)
 text_str = text_binary.decode("windows-1251")
 
+cipher = VigenereCipher(text_str, password)
+encrypted_text = cipher.encrypt()
+text_back_to_str = encrypted_text.encode("windows-1251")
+plot_histogram(text_back_to_str)
+
+
+# second task
+password = "10"
+
+cipher = VigenereCipher(text_str, password)
+encrypted_text = cipher.encrypt()
+text_back_to_str = encrypted_text.encode("windows-1251")
+plot_histogram(text_back_to_str)
+
+# third task
+password = "Поле глазасто, а лес ушаст."
+text_str = text_binary.decode("windows-1251").upper()
+
+cipher = VigenereCipher(text_str, password)
+encrypted_text = cipher.encrypt()
+text_back_to_str = encrypted_text.encode("windows-1251")
+plot_histogram(text_back_to_str)
+
+
+# fourth task
+fpoly = [5,2]
+L = LFSR(fpoly=fpoly, initstate='random')
+
+k=1000*8
+seq_k  = L.runKCycle(k)
+
+password = L.arr2str(seq_k)
 cipher = VigenereCipher(text_str, password)
 encrypted_text = cipher.encrypt()
 text_back_to_str = encrypted_text.encode("windows-1251")
