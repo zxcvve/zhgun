286e3aede496769e5d4213506679770571ab4f45
Serafim Lyovin <87949621+zxcvve@users.noreply.github.com>
2024-06-13 22:10:54 +0300

    lab3: add ability to specify histogram title, output histogram of the text before encryption

--- lab3/main.py
+++ lab3/main.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
 from pylfsr import LFSR
 from collections import Counter
-import numpy as np
 import os
 
 script_path = os.path.abspath(__file__)
 script_dir = os.path.dirname(script_path)
+
 
 class VigenereCipher:
     def __init__(self, text, password):
@@ -17,39 +17,49 @@ class VigenereCipher:
         extended_password = self.password
         while len(extended_password) < len(self.text):
             extended_password += self.password
-        return extended_password[:len(self.text)]
+        return extended_password[: len(self.text)]
 
     def encrypt(self):
         encrypted_text = []
         for i in range(len(self.text)):
             text_char = self.text[i]
             password_char = self.extended_password[i]
-            if text_char.isalpha():  
-                shift = ord(password_char.lower()) - ord('а')
+            if text_char.isalpha():
+                shift = ord(password_char.lower()) - ord("а")
                 if text_char.islower():
-                    encrypted_char = chr((ord(text_char) - ord('а') + shift) % 32 + ord('а'))
+                    encrypted_char = chr(
+                        (ord(text_char) - ord("а") + shift) % 32 + ord("а")
+                    )
                 else:
-                    encrypted_char = chr((ord(text_char) - ord('А') + shift) % 32 + ord('А'))
+                    encrypted_char = chr(
+                        (ord(text_char) - ord("А") + shift) % 32 + ord("А")
+                    )
                 encrypted_text.append(encrypted_char)
             else:
                 encrypted_text.append(text_char)
-        return ''.join(encrypted_text)
+        return "".join(encrypted_text)
 
 
 def read_file(file_path):
     with open(file_path, "rb") as file:
         return file.read()
 
-def plot_histogram(text_bin):
+
+def plot_histogram(text_bin, title="Text Histogram"):
     frequency = Counter(text_bin)
     labels, values = zip(*frequency.most_common())
     plt.bar(labels, values)
     plt.xlabel("ASCII value")
     plt.ylabel("Frequency")
-    plt.title("Text Histogram")
+    plt.title(title)
     plt.show()
 
-file_path = f"{script_dir}/input.txt" 
+
+file_path = f"{script_dir}/input.txt"
+
+text_binary = read_file(file_path)
+plot_histogram(text_binary, "Текст до шифрования")
+
 
 # first task
 password = "Поле глазасто, а лес ушаст."
@@ -59,7 +69,10 @@ text_str = text_binary.decode("windows-1251")
 cipher = VigenereCipher(text_str, password)
 encrypted_text = cipher.encrypt()
 text_back_to_str = encrypted_text.encode("windows-1251")
-plot_histogram(text_back_to_str)
+plot_histogram(
+    text_back_to_str,
+    "Текст после шифрования (шифр Виженера)",
+)
 
 
 # second task
@@ -81,11 +94,11 @@ plot_histogram(text_back_to_str)
 
 
 # fourth task
-fpoly = [5,2]
-L = LFSR(fpoly=fpoly, initstate='random')
+fpoly = [5, 2]
+L = LFSR(fpoly=fpoly, initstate="random")
 
-k=1000*8
-seq_k  = L.runKCycle(k)
+k = 1000 * 8
+seq_k = L.runKCycle(k)
 
 text_str = text_binary.decode("windows-1251")
 password = L.arr2str(seq_k)
