d09f19e9991cdaade733df0ba14bfbe36f4445c8
Serafim Lyovin <87949621+zxcvve@users.noreply.github.com>
2024-06-13 22:49:30 +0300

    lab3: add caesar cipher

--- lab3/main.py
+++ lab3/main.py
@@ -55,42 +55,76 @@ def plot_histogram(text_bin, title="Text Histogram"):
     plt.show()
 
 
+def plot_multiple_histograms(text_bin1, text_bin2, title="Text Histogram"):
+    frequency1 = Counter(text_bin1)
+    frequency2 = Counter(text_bin2)
+    labels, values = zip(*frequency1.most_common())
+    plt.bar(labels, values)
+    labels, values = zip(*frequency2.most_common())
+    plt.bar(labels, values)
+    plt.xlabel("ASCII value")
+    plt.ylabel("Frequency")
+    plt.title(title)
+    plt.show()
+
+
+class CaesarCipher:
+    def __init__(self, text, shift):
+        self.text = text
+        self.shift = shift
+
+    def encrypt(self):
+        encrypted_text = []
+        for char in self.text:
+            if char.isalpha():
+                shift = self.shift % 32
+                if char.islower():
+                    encrypted_char = chr((ord(char) - ord("а") + shift) % 32 + ord("а"))
+                else:
+                    encrypted_char = chr((ord(char) - ord("А") + shift) % 32 + ord("А"))
+                encrypted_text.append(encrypted_char)
+            else:
+                encrypted_text.append(char)
+        return "".join(encrypted_text)
+
+
 file_path = f"{script_dir}/input.txt"
 
-text_binary = read_file(file_path)
-plot_histogram(text_binary, "Текст до шифрования")
+# вывод гистограммы для текста до шифрования
+init_file = read_file(file_path)
+plot_histogram(init_file, "Текст до шифрования")
 
 
 # first task
 password = "Поле глазасто, а лес ушаст."
-text_binary = read_file(file_path)
-text_str = text_binary.decode("windows-1251")
+init_file = read_file(file_path)
+text_str = init_file.decode("windows-1251")
 
 cipher = VigenereCipher(text_str, password)
 encrypted_text = cipher.encrypt()
-text_back_to_str = encrypted_text.encode("windows-1251")
+caesar_encrypted = encrypted_text.encode("windows-1251")
 plot_histogram(
-    text_back_to_str,
+    caesar_encrypted,
     "Текст после шифрования (шифр Виженера)",
 )
 
 
 # second task
-password = "10"
+shift = 10
 
-cipher = VigenereCipher(text_str, password)
+cipher = CaesarCipher(text_str, shift)
 encrypted_text = cipher.encrypt()
-text_back_to_str = encrypted_text.encode("windows-1251")
-plot_histogram(text_back_to_str)
+caesar_encrypted = encrypted_text.encode("windows-1251")
+plot_histogram(caesar_encrypted, "Текст после шифрования (шифр Цезаря)")
 
 # third task
 password = "Поле глазасто, а лес ушаст."
-text_str = text_binary.decode("windows-1251").upper()
+text_str = init_file.decode("windows-1251").upper()
 
 cipher = VigenereCipher(text_str, password)
 encrypted_text = cipher.encrypt()
-text_back_to_str = encrypted_text.encode("windows-1251")
-plot_histogram(text_back_to_str)
+z32 = encrypted_text.encode("windows-1251")
+plot_histogram(z32, "Текст после шифрования (Z32)")
 
 
 # fourth task
@@ -100,9 +134,9 @@ L = LFSR(fpoly=fpoly, initstate="random")
 k = 1000 * 8
 seq_k = L.runKCycle(k)
 
-text_str = text_binary.decode("windows-1251")
+text_str = init_file.decode("windows-1251")
 password = L.arr2str(seq_k)
 cipher = VigenereCipher(text_str, password)
 encrypted_text = cipher.encrypt()
-text_back_to_str = encrypted_text.encode("windows-1251")
-plot_histogram(text_back_to_str)
+lfsr_encrypted = encrypted_text.encode("windows-1251")
+plot_histogram(lfsr_encrypted, "Текст после шифрования (LFSR)")
